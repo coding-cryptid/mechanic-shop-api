50f788a8d4b28109f5aa7843e638195b025b84f4
Gianna Cosenza <giacosey28@gmail.com>
2026-08-03 12:54:39 -0400

    fixed missing issues that were overlooked during the ERD switch

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing import List
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
@@ -11,7 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(model_class = Base)
+db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 class Customer(Base):
@@ -22,26 +26,26 @@ class Customer(Base):
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     phone_number: Mapped[str] = mapped_column(nullable=False)
 
-    service_tickets: Mapped[List['Service_Tickets']] = mapped_column(back_populates='customer')
+    service_tickets: Mapped[List['Service_Tickets']] = relationship(back_populates='customer')
 
-service_mechanics = db.Table (
+service_mechanics = db.Table(
     'service_mechanics',
     Base.metadata,
-    db.Column('service_ticket_id', db.ForeignKey('service_tickets.id')),
-    db.Column('mechanic_id', db.ForeignKey('mechanics.id'))
+    db.Column('service_ticket_id', db.ForeignKey('service_tickets.id'), primary_key=True),
+    db.Column('mechanic_id', db.ForeignKey('mechanics.id'), primary_key=True)
 )
 
 class Service_Tickets(Base):
     __tablename__ = 'service_tickets'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    customer_id: Mapped[int] = mapped_column(nullable=False)
+    customer_id: Mapped[int] = mapped_column(ForeignKey('customers.id'), nullable=False)
     vin: Mapped[str] = mapped_column(nullable=False)
     service_date: Mapped[str] = mapped_column(nullable=False)
     service_description: Mapped[str] = mapped_column(nullable=False)
 
-    customer: Mapped['Customer'] = mapped_column(back_populates='service_tickets')
-    mechanics: Mapped[List['Mechanics']] = mapped_column(secondary=service_mechanics, back_populates='service_tickets')
+    customer: Mapped['Customer'] = relationship(back_populates='service_tickets')
+    mechanics: Mapped[List['Mechanics']] = relationship(secondary=service_mechanics, back_populates='service_tickets')
 
 class Mechanics(Base):
     __tablename__ = 'mechanics'
@@ -52,8 +56,10 @@ class Mechanics(Base):
     phone: Mapped[str] = mapped_column(nullable=False)
     salary: Mapped[float] = mapped_column(nullable=False)
 
-    service_tickets: Mapped[List['Service_Tickets']] = mapped_column(secondary=service_mechanics, back_populates='mechanics')
+    service_tickets: Mapped[List['Service_Tickets']] = relationship(secondary=service_mechanics, back_populates='mechanics')
 
 with app.app_context():
-			db.create_all()		
-app.run()
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run()
