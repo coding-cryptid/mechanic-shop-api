5540999ba1c704ab42f3702b1fb74a80ace87823
Gianna Cosenza <giacosey28@gmail.com>
2026-08-03 11:09:39 -0400

    PROJECT REQUIREMENTS CHANGED. EDITED TABLES TO MATCH THE NEW ERD.

--- app.py
+++ app.py
@@ -21,50 +21,30 @@ class Customer(Base):
     __tablename__ = 'customers'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    first_name: Mapped[str] = mapped_column(nullable=False)
-    last_name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     phone_number: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped[str] = mapped_column(nullable=False)
 
-    vehicles: Mapped[List['Vehicle']] = mapped_column(back_populates='customers')
-
-class Vehicle(Base):
-    __tablename__ = 'vehicles'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    customer_id: Mapped[int] = mapped_column(nullable=False)
-    vin: Mapped[str] = mapped_column(nullable=False, unique=True)
-    make: Mapped[str] = mapped_column(nullable=False)
-    model: Mapped[str] = mapped_column(nullable=False)
-    year: Mapped[int] = mapped_column(nullable=False)
-    mileage: Mapped[int] = mapped_column(nullable=False)
-
-    customer: Mapped['Customer'] = mapped_column(back_populates='vehicles')
-    service_tickets: Mapped[List['Service_Tickets']] = mapped_column(back_populates='vehicles')
+    service_tickets: Mapped[List['Service_Tickets']] = mapped_column(back_populates='customer')
 
 class Service_Tickets(Base):
     __tablename__ = 'service_tickets'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    vehicle_id: Mapped[int] = mapped_column(nullable=False)
-    date_in: Mapped[str] = mapped_column(nullable=False)
-    date_out: Mapped[str] = mapped_column(nullable=True)
+    customer_id: Mapped[int] = mapped_column(nullable=False)
+    vin: Mapped[str] = mapped_column(nullable=False)
+    service_date: Mapped[str] = mapped_column(nullable=False)
     service_description: Mapped[str] = mapped_column(nullable=False)
-    status: Mapped[str] = mapped_column(nullable=False)
-    cost: Mapped[float] = mapped_column(nullable=False)
 
-    vehicle: Mapped['Vehicle'] = mapped_column(back_populates='service_tickets')
+    customer: Mapped['Customer'] = mapped_column(back_populates='service_tickets')
 
 class Mechanics(Base):
     __tablename__ = 'mechanics'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    first_name: Mapped[str] = mapped_column(nullable=False)
-    last_name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     phone: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped[str] = mapped_column(nullable=False)
     salary: Mapped[float] = mapped_column(nullable=False)
 
 with app.app_context():
