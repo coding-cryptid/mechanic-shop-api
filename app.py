138d71a56e827c8b97bb8aa03ac405a4c0d85b18
Gianna Cosenza <giacosey28@gmail.com>
2026-08-03 10:54:05 -0400

    Created tables

--- app.py
+++ app.py
@@ -26,6 +26,39 @@ class Customer(Base):
     phone_number: Mapped[str] = mapped_column(nullable=False)
     address: Mapped[str] = mapped_column(nullable=False)
 
+class Vehicle(Base):
+    __tablename__ = 'vehicles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    customer_id: Mapped[int] = mapped_column(nullable=False)
+    vin: Mapped[str] = mapped_column(nullable=False, unique=True)
+    make: Mapped[str] = mapped_column(nullable=False)
+    model: Mapped[str] = mapped_column(nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
+    mileage: Mapped[int] = mapped_column(nullable=False)
+
+class Service_Tickets(Base):
+    __tablename__ = 'service_tickets'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    vehicle_id: Mapped[int] = mapped_column(nullable=False)
+    date_in: Mapped[str] = mapped_column(nullable=False)
+    date_out: Mapped[str] = mapped_column(nullable=True)
+    service_description: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(nullable=False)
+    cost: Mapped[float] = mapped_column(nullable=False)
+
+class Mechanics(Base):
+    __tablename__ = 'mechanics'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    phone: Mapped[str] = mapped_column(nullable=False)
+    address: Mapped[str] = mapped_column(nullable=False)
+    salary: Mapped[float] = mapped_column(nullable=False)
+
 with app.app_context():
 			db.create_all()		
 app.run()
