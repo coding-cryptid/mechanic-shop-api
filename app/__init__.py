8e249d6e8a9c831e26844d278826d5ec5a445940
Gianna Cosenza <giacosey28@gmail.com>
2026-08-18 11:55:16 -0400

    imports and flask swagger ui

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,18 @@ from .blueprints.service_tickets import service_tickets_bp
 from .blueprints.mechanics import mechanics_bp
 from .blueprints.inventory import inventory_bp
 from app.blueprints.users import users_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Mechanic Shop API"
+    }
+)
 
 load_dotenv()
 
@@ -31,5 +43,6 @@ def create_app(config_name="DevelopmentConfig"):
     app.register_blueprint(mechanics_bp, url_prefix="/mechanics")
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
     app.register_blueprint(users_bp, url_prefix='/users')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
